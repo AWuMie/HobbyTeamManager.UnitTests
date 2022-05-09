fc6a0c00bdf1df1fe3a2f2d69b04a73f38393847
Achim Mienhardt <59267410+AWuMie@users.noreply.github.com>
2022-05-09 14:20:26 +0200

    removed unnecessary usings

--- UnitTests/Player/PlayerDeleteModelTests.cs
+++ UnitTests/Player/PlayerDeleteModelTests.cs
@@ -5,10 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using NUnit.Framework;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace HobbyTeamManager.UnitTests.UnitTests.Player;
